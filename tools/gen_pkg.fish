66433df86adbb32e700a3e4e365961e07cc9cbd1
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-11-11 21:13:39 +0300

    weird manjaro bug

--- tools/gen_pkg.fish
+++ tools/gen_pkg.fish
@@ -4,22 +4,22 @@ cd ../
 [ ! -d "$DESTDIR" ]; and echo "No such directory: $DESTDIR"
 [ ! -d "$DESTDIR" ]; and exit
 
-mkdir -p $DESTDIR/usr $DESTDIR/usr/share $DESTDIR/usr/share/xsessions
-cp xsessions/unityx.desktop $DESTDIR/usr/share/xsessions
+sudo mkdir -p /usr/share/xsessions
+sudo cp xsessions/unityx.desktop /usr/share/xsessions
 
-mkdir -p $DESTDIR/usr/share/applications
-cp applications/* $DESTDIR/usr/share/applications
+sudo mkdir /usr/share/applications
+sudo cp applications/* /usr/share/applications
 
-mkdir -p $DESTDIR/usr/share/unityx
-cp -r ./*/ $DESTDIR/usr/share/unityx
+sudo mkdir -p /usr/share/unityx
+sudo cp -r ./*/ /usr/share/unityx
 
-rm -r $DESTDIR/usr/share/unityx/xsessions
+sudo rm -r /usr/share/unityx/xsessions
 
-rm -r $DESTDIR/usr/share/unityx/xfce4
-cp unityx $DESTDIR/usr/share/unityx
+sudo rm -r /usr/share/unityx/xfce4
+sudo cp unityx /usr/share/unityx
 
-sudo cp -r $DESTDIR/usr/share/unityx/Adwaita-dark-Xfce /usr/share/themes/
-rm -r $DESTDIR/usr/share/unityx/Adwaita-dark-Xfce
+sudo cp -r /usr/share/unityx/Adwaita-dark-Xfce /usr/share/themes/
+sudo rm -r /usr/share/unityx/Adwaita-dark-Xfce
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark-Xfce"
 
