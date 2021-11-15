b4e2365af423755c8388768f61d33b807140a3f6
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-11-15 21:18:13 +0300

    Update gen_pkg.fish

--- tools/gen_pkg.fish
+++ tools/gen_pkg.fish
@@ -17,10 +17,15 @@ sudo rm -r /usr/share/unityx/xsessions
 
 sudo cp unityx /usr/share/unityx
 
-sudo cp -r /usr/share/unityx/Adwaita-dark-Xfce /usr/share/themes/
-sudo rm -r /usr/share/unityx/Adwaita-dark-Xfce
+sudo cp -r ./config/xfce4/ ~/.config/
+sudo cp -r ./config/Adwaita-dark-Xfce /usr/share/themes/
+sudo cp -r ./config/Flat-Remix-Yellow-Dark /usr/share/icons/
+sudo cp -r ./config/gtk-3.0 ~/.config/
+sudo cp ./config/.gtkrc-2.0 ~/
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark-Xfce"
+gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Yellow-Dark"
 
-#sudo cp -r xfce4/ ~/.config/
+sudo rm -r /usr/share/unityx/config
+
 sudo chmod +x /usr/share/unityx/unityx
